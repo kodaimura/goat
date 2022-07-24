e547721c1d94f4015841e91ac4032bf537b549ed
kodaimura <kodaimura123@gmail.com>
2022-07-24 21:51:32 +0900

    1.0.0

--- goat/internal/model/entity/user.go
+++ goat/internal/model/entity/user.go
@@ -2,9 +2,9 @@ package entity
 
 
 type User struct {
-	UserId int `db:"USER_ID" json:"userid"`
+	UserId int `db:"USER_ID" json:"user_id"`
 	Username string `db:"USERNAME" json:"username"`
 	Password string `db:"PASSWORD" json:"password"`
-	CreateAt string `db:"CREATE_AT" json:"createat"`
-	UpdateAt string `db:"UPDATE_AT" json:"updateat"`
+	CreateAt string `db:"CREATE_AT" json:"create_at"`
+	UpdateAt string `db:"UPDATE_AT" json:"update_at"`
 }
