2f89c80a4fb0bbd16b72962166b87f5154f90b8e
kodaimura <kodaimura123@gmail.com>
2023-12-30 23:49:23 +0900

    fix JWT_EXPIRES

--- goat/internal/core/jwt/constants.go
+++ goat/internal/core/jwt/constants.go
@@ -6,4 +6,4 @@ import (
 
 const COOKIE_KEY_JWT string = "access_token"
 const CONTEXT_KEY_PAYLOAD string = "payload"
-const JWT_EXPIRES time.Duration = 108000
+const JWT_EXPIRES time.Duration = 30 * 24 * 60 * 60
