5201323e091a621dd1e391f46eb58ef3244189d8
kodaimura <kodaimura123@gmail.com>
2024-04-27 13:20:52 +0900

    update mail

--- internal/core/mail/mail.go
+++ internal/core/mail/mail.go
@@ -9,19 +9,29 @@ import (
 )
 
 
-func Send (from string, to []string, subject, body string) {
-	cf := config.GetConfig()
-	auth := smtp.PlainAuth("", cf.MailUser, cf.MailPass, cf.MailHost)
+func Send (from string, to []string, subject, body string) error {
+	address := getSmtpAddress()
+	auth := getSmtpAuth()
+	msg := generateMessageByte(from, to, subject, body)
+	return smtp.SendMail(address, auth, from, to, msg)
+}
 
+func getSmtpAuth () smtp.Auth {
+	cf := config.GetConfig()
+	return smtp.PlainAuth("", cf.MailUser, cf.MailPass, cf.MailHost)
+}
+
+func getSmtpAddress () string {
+	cf := config.GetConfig()
+	return fmt.Sprintf("%s:%s", cf.MailHost, cf.MailPort)
+}
+
+func generateMessageByte (from string, to []string, subject, body string) []byte {
 	header := "From: " + from + "\r\n" +
 		"To: " + strings.Join(to, ", ") + "\r\n" +
 		"Subject: " + mime.QEncoding.Encode("UTF-8", subject) + "\r\n" +
 		"MIME-Version: 1.0\r\n" +
 		"Content-Type: text/plain; charset=UTF-8\r\n"
 
-	mail := header + "\r\n" + body
-
-    if err := smtp.SendMail(fmt.Sprintf("%s:%s", cf.MailHost, cf.MailPort), auth, from, to, []byte(mail)); err != nil {
-        fmt.Println("Error sending email:", err)
-    }
+	return []byte(header + "\r\n" + body)
 }
