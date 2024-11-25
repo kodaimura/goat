fefb36d2094f65283591b811bca4a63c3e195ffa
kodaimura <kodaimura123@gmail.com>
2024-11-25 22:28:25 +0900

    regexp.MustCompile

--- template/internal/service/common.go
+++ template/internal/service/common.go
@@ -1,7 +1,7 @@
 package service
 
 import (
-    "strings"
+	"regexp"
     "github.com/lib/pq"
     "github.com/go-sql-driver/mysql"
     "github.com/mattn/go-sqlite3"
@@ -14,25 +14,22 @@ func GetConflictColumn(err error) (string, bool) {
     }
 
 	if pgErr, ok := err.(*pq.Error); ok && pgErr.Code == "23505" {
-        if pgErr.Detail != "" {
-			parts := strings.Split(pgErr.Detail, "violates unique constraint")
-			if len(parts) > 0 {
-				return strings.TrimSpace(parts[0]), true
-			}
+        re := regexp.MustCompile(`Key \((\w+)\)=`)
+		match := re.FindStringSubmatch(pgErr.Detail)
+		if len(match) > 1 {
+			return match[1], true
 		}
     } else if mysqlErr, ok := err.(*mysql.MySQLError); ok && mysqlErr.Number == 1062 {
-        if strings.Contains(mysqlErr.Message, "for key") {
-			parts := strings.Split(mysqlErr.Message, "for key")
-			if len(parts) > 1 {
-				return strings.TrimSpace(parts[1]), true
-			}
+        re := regexp.MustCompile(`for key '([\w.]+)'`)
+		match := re.FindStringSubmatch(mysqlErr.Message)
+		if len(match) > 1 {
+			return match[1], true
 		}
     } else if sqliteErr, ok := err.(sqlite3.Error); ok && sqliteErr.Code == sqlite3.ErrConstraint {
-        if strings.Contains(sqliteErr.Error(), "UNIQUE constraint failed") {
-			parts := strings.Split(sqliteErr.Error(), ":")
-			if len(parts) > 1 {
-				return strings.TrimSpace(parts[1]), true
-			}
+        re := regexp.MustCompile(`UNIQUE constraint failed: ([\w.]+)`)
+		match := re.FindStringSubmatch(sqliteErr.Error())
+		if len(match) > 1 {
+			return match[1], true
 		}
     }
 
